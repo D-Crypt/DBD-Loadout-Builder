115866d14b8c48d276551ad9243431d188a25a62
D-Crypt <danielvaughan54@gmail.com>
2021-08-19 16:06:09 +0100

    Add A-C perks to perk list

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 class Perk {
-    constructor(name, type, character) {
+    constructor(name, character, type) {
         this.name = name;
-        this.type = type;
         this.character = character;
+        this.type = type;
     }
 }
 
+const perkList = [
+    new Perk("Adrenaline", "Meg", ["Healing", "Exhaustion", "Movement speed"]),
+    new Perk("Adrenaline", "Meg", ["Healing", "Exhaustion", "Movement speed"]),
+    new Perk("Aftercare", "Jeff", ["Aura", "Hook", "Healing"]),
+    new Perk("Alert", "Feng", ["Pallet", "Wall", "Generator", "Aura"]),
+    new Perk("Any Means Necessary", "Yui", ["Pallet"]),
+    new Perk("Appraisal", "Elodie", ["Chest", "Item", "Token"]),
+    new Perk("Autodidact", "Adam", ["Skill Checks", "Healing", "Token"]),
+
+    new Perk("Babysitter", "Steve", ["Hook", "Stealth", "Aura"]),
+    new Perk("Balanced Landing", "Nea", ["Exhaustion", "Movement speed"]),
+    new Perk("Better Together", "Nancy", ["Generator", "Aura"]),
+    new Perk("Bite The Bullet", "Leon", ["Healing", "Stealth", "Skill Checks"]),
+    new Perk("Blast Mine", "Jill", ["Generator", "Stun", "Aura"]),
+    new Perk("Blood Pact", "Cheryl", ["Obsession", "Aura", "Healing", "Movement speed"]),
+    new Perk("Boil Over", "Kate", ["Wiggle speed", "Aura", "Hook"]),
+    new Perk("Bond", "Dwight", ["Aura"]),
+    new Perk("Botany Knowledge", "Claudette", ["Healing", "Item"]),
+    new Perk("Breakdown", "Jeff", ["Hook", "Aura"]),
+    new Perk("Breakout", "Yui", ["Movement speed", "Wiggle speed"]),
+    new Perk("Buckle Up", "Ash", ["Aura", "Healing"]),
+    new Perk("Built To Last", "Felix", ["Item"]),
+
+    new Perk("Calm Spirit", "Jake", ["Stealth"]),
+    new Perk("Camaraderie", "Steve", ["Hook"]),
+    new Perk("Counterforce", "Jill", ["Totem", "Aura"]),
+];
